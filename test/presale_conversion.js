c8c42e0033a0dd3ea58e90eb2b49b5fe8515e3c9
Sergey Vasilyev <sergey.vasilyev@dataart.com>
2017-10-16 22:51:27 +0300

    Presale conversion test doesn't depend on migrations

--- test/presale_conversion.js
+++ test/presale_conversion.js
@@ -1,39 +1,31 @@
-const BigNumber = web3.BigNumber;
+const BigNumber = web3.BigNumber
 
 const should = require('chai')
   .use(require('chai-as-promised'))
   .use(require('chai-bignumber')(BigNumber))
-  .should();
+  .should()
 
-const TokenEscrow = artifacts.require('test/TokenEscrow.sol'); // PBKX token
-const BankexToken = artifacts.require('BankexToken');
-const BankexCrowdsale = artifacts.require('BankexCrowdsale');
+const TokenEscrow = artifacts.require('test/TokenEscrow.sol') // PBKX token
+const BankexToken = artifacts.require('BankexToken')
 
-contract('PresaleConversion', function ([owner, investor, _]) {
+contract('PresaleConversion', function ([_, bankexTokenWallet, pbkxToken, pbkxInvestor]) {
 
-  before(async function() {
-    // this.pbkxToken = await TokenEscrow.new();
-    // this.presaleConversion = await PresaleConversion.new(this.pbkxToken.address);
-    // this.bkxToken = await BankexToken.new(this.presaleConversion.address);
-    // await this.pbkxToken.setToken(this.presaleConversion.address);
-    // await this.pbkxToken.setRate(1);
-    // await this.presaleConversion.setBkxAddress(this.bkxToken.address);
-    this.pbkxToken = await TokenEscrow.deployed();
-    this.crowdsale = await BankexCrowdsale.deployed();
-    this.bkxToken = BankexToken.at(await this.crowdsale.token());
-  });
+  beforeEach(async function() {
+    this.pbkxToken = await TokenEscrow.new()
+    this.bkxToken = await BankexToken.new(bankexTokenWallet, this.pbkxToken.address, 1)
+    await this.pbkxToken.setToken(this.bkxToken.address)
+    await this.pbkxToken.setRate(10 ** 16)
+  })
 
-  it("should convert PBKX to BKX", async function() {
-    await this.pbkxToken.sendTransaction({from: investor, value: web3.toWei(1, "ether")});
-    const pbkxBalance = await this.pbkxToken.balanceOf(investor);
-    const pbkxDecimals = await this.pbkxToken.decimals();
-    const pbkxTokens = pbkxBalance.div(new BigNumber(10).pow(pbkxDecimals));
-    const oldBkxBalance = await this.bkxToken.balanceOf(investor);
-    await this.pbkxToken.convert({from: investor});
-    const newBkxBalance = await this.bkxToken.balanceOf(investor);
-    const diff = newBkxBalance.sub(oldBkxBalance);
-    const bkxDecimals = await this.bkxToken.decimals();
-    const bkxTokens = diff.div(new BigNumber(10).pow(bkxDecimals));
-    bkxTokens.should.be.bignumber.equal(pbkxTokens);
-  });
+  it('should convert 1 PBKX to 1 BKX', async function() {
+    await this.pbkxToken.sendTransaction({from: pbkxInvestor, value: web3.toWei(1, 'ether')})
+    const pbkxBalance = await this.pbkxToken.balanceOf(pbkxInvestor)
+    const pbkxDecimals = await this.pbkxToken.decimals()
+    const pbkxTokens = pbkxBalance.div(new BigNumber(10).pow(pbkxDecimals))
+    await this.pbkxToken.convert({from: pbkxInvestor})
+    const bkxBalance = await this.bkxToken.balanceOf(pbkxInvestor)
+    const bkxDecimals = await this.bkxToken.decimals()
+    const bkxTokens = bkxBalance.div(new BigNumber(10).pow(bkxDecimals))
+    bkxTokens.should.be.bignumber.equal(pbkxTokens)
+  })
 })
